07e1306cd7c8eb00520958213ee3c054a7053cf6
alex <135154483+ASLarin@users.noreply.github.com>
2024-10-26 01:06:09 +0300

    Update int2str.c

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,37 +1,38 @@
 #include "int2str.h"
 #include <stdbool.h>
+#include <stdlib.h>
 #define int_MIN (-2147483647 - 1)
 #define int_MAX 2147483647
 char* int2str(int number) {
-  static char buffer[12];
-  long int h = number;
+  char* str = (char*)malloc(12 * sizeof(char));
+
 
   bool is_negative = false;
   int i = 0;
+  long long int remainder = number;
 
-  if (h < 0) {
+  if (remainder < 0) {
     is_negative = true;
-    h = -h;
+    remainder = -remainder;
   }
-  if (h == 0) {
-    buffer[i++] = '0';
-    buffer[i] = '\0';
-    return buffer;
+  if (remainder == 0) {
+    str[i++] = '0';
+    str[i] = '\0';
+    return str;
   }
-  while (h != 0) {
-    buffer[i++] = (h % 10) + '0';
-    h /= 10;
+  while (remainder != 0) {
+    str[i++] = (remainder % 10) + '0';
+    remainder /= 10;
   }
   if (is_negative) {
-    buffer[i++] = '-';
+    str[i++] = '-';
   }
-  buffer[i] = '\0';
+  str[i] = '\0';
   for (int j = 0; j < i / 2; j++) {
-    char temp = buffer[j];
-    buffer[j] = buffer[i - j - 1];
-    buffer[i - j - 1] = temp;
+    char temp = str[j];
+    str[j] = str[i - j - 1];
+    str[i - j - 1] = temp;
   }
 
-  return buffer;
+  return str;
 }
-
