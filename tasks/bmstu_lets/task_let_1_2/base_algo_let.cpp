4d4c47fc68183e41d9bd54dba434f4c4c30e30e0
ASLarin <135154483+ASLarin@users.noreply.github.com>
2025-03-01 15:15:22 +0300

    летучка style_check

--- tasks/bmstu_lets/task_let_1_2/base_algo_let.cpp
+++ tasks/bmstu_lets/task_let_1_2/base_algo_let.cpp
@@ -1,25 +1,48 @@
 #include <algorithm>
 #include "base_algo_let.h"
 
-std::vector<int> positive_numbers(const std::vector<int>& v) {
-  return {};
+
+std::vector<int> positive_numbers(const std::vector<int>& nums) {
+  std::vector<int> result;
+  std::copy_if(nums.begin(), nums.end(), std::back_inserter(result), [](int value) { return value > 0; });
+  return result;
 }
-void sort_positive_numbers(std::vector<int>& v) {
+
+void sort_positive_numbers(std::vector<int>& nums) {
+
+  auto partition_point = std::partition(nums.begin(), nums.end(), [](int num) {
+      return num > 0;
+  });
+  return std::sort(nums.begin(), partition_point);
 }
-int sum_positive_numbers(const std::vector<int>& v) {
-  return 0;
+
+int sum_positive_numbers(const std::vector<int>& nums) {
+  int sum = 0;
+  std::for_each(nums.begin(), nums.end(), [&sum](int x) {
+      if (x > 0) {
+          sum += x;
+      }
+  });
+  return sum;
 }
-bool is_divisible_by_10(const std::vector<int>& v) {
-  return false;
+
+
+bool is_divisible_by_10(const std::vector<int>& nums) {
+  return std::any_of(nums.begin(), nums.end(), [](int x) { return x % 10 == 0; });
 }
-void replace_negative_numbers(std::vector<int>& v) {
-  return;
+
+void replace_negative_numbers(std::vector<int>& nums) {
+  std::replace_if(nums.begin(), nums.end(), [](int value) { return value < 0; }, 0);
 }
-void double_values(std::vector<int>& v) {
-  return;
+
+void double_values(std::vector<int>& nums) {
+  std::for_each(nums.begin(), nums.end(), [](int& value) { value *= 2; });
 }
-void sort_students_by_age(std::vector<Student>& v) {
-  return;
+
+void sort_students_by_age(std::vector<Student>& data) {
+  std::sort(data.begin(), data.end(), [](const Student& a, const Student& b) { return a.age < b.age; });
 }
-void sort_students_by_name(std::vector<Student>& v) {
+
+void sort_students_by_name(std::vector<Student>& students) {
+  std::sort(students.begin(), students.end(), [](const Student& a, const Student& b) { return a.name < b.name; });
 }
