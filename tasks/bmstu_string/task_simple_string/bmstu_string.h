3a2a0ae73377527662a00858176e0de08848fdee
ASLarin <135154483+ASLarin@users.noreply.github.com>
2025-03-22 12:32:51 +0300

    исправление стиля

--- tasks/bmstu_string/task_simple_string/bmstu_string.h
+++ tasks/bmstu_string/task_simple_string/bmstu_string.h
@@ -4,7 +4,8 @@
 #include <iostream>
 
 namespace bmstu {
-template <typename T> class basic_string;
+template <typename T>
+class basic_string;
 
 using string = basic_string<char>;
 using wstring = basic_string<wchar_t>;
@@ -12,7 +13,8 @@ using u8string = basic_string<char8_t>;
 using u16string = basic_string<char16_t>;
 using u32string = basic_string<char32_t>;
 
-template <typename T> class basic_string {
+template <typename T>
+class basic_string {
 public:
   basic_string() : ptr_(new T[1]{0}), size_(0) {}
 
@@ -23,7 +25,8 @@ public:
     ptr_[size] = T(0);
   }
 
-  basic_string(std::initializer_list<T> il) : ptr_(new T[il.size() + 1]), size_(il.size()) {
+  basic_string(std::initializer_list<T> il)
+      : ptr_(new T[il.size() + 1]), size_(il.size()) {
     T* p = ptr_;
     for (const auto& c : il) {
       *p++ = c;
@@ -61,17 +64,11 @@ public:
     std::swap(size_, dying.size_);
   }
 
-  ~basic_string() {
-    delete[] ptr_;
-  }
+  ~basic_string() { delete[] ptr_; }
 
-  const T* c_str() const {
-    return ptr_;
-  }
+  const T* c_str() const { return ptr_; }
 
-  size_t size() const {
-    return size_;
-  }
+  size_t size() const { return size_; }
 
   basic_string& operator=(basic_string&& other) noexcept {
     if (this != &other) {
@@ -108,58 +105,62 @@ public:
     return *this;
   }
 
-  friend basic_string<T> operator+(const basic_string<T>& left, const basic_string<T>& right) {
+  friend basic_string<T> operator+(const basic_string<T>& left,
+                                   const basic_string<T>& right) {
     basic_string<T> result;
     result.size_ = left.size_ + right.size_;
     delete[] result.ptr_;
     result.ptr_ = new T[result.size_ + 1];
-    
+
     // Копируем левую строку
     for (size_t i = 0; i < left.size_; ++i) {
       result.ptr_[i] = left.ptr_[i];
     }
-    
+
     // Копируем правую строку
     for (size_t i = 0; i < right.size_; ++i) {
       result.ptr_[left.size_ + i] = right.ptr_[i];
     }
-    
+
     result.ptr_[result.size_] = T(0);
     return result;
   }
 
-  friend std::basic_ostream<T> &operator<<(std::basic_ostream<T> &os, const basic_string &obj) {
-     os << obj.c_str(); 
-     return os; 
-    }
+  friend std::basic_ostream<T>& operator<<(std::basic_ostream<T>& os,
+                                           const basic_string& obj) {
+    os << obj.c_str();
+    return os;
+  }
 
-friend std::basic_istream<T> &operator>>(std::basic_istream<T> &is, basic_string &obj) 
-{ is >> std::noskipws;
-T symbol = 1; 
-for (size_t i = 0; symbol != 0; ++i) { 
-  symbol = 0; is >> symbol; 
-  if (symbol != 0) { 
-    obj += symbol; 
-    } 
-  } 
-  return is;
- }
+  friend std::basic_istream<T>& operator>>(std::basic_istream<T>& is,
+                                           basic_string& obj) {
+    is >> std::noskipws;
+    T symbol = 1;
+    for (size_t i = 0; symbol != 0; ++i) {
+      symbol = 0;
+      is >> symbol;
+      if (symbol != 0) {
+        obj += symbol;
+      }
+    }
+    return is;
+  }
 
   basic_string& operator+=(const basic_string& other) {
     if (other.size_ > 0) {
       size_t new_size = size_ + other.size_;
       T* new_ptr = new T[new_size + 1];
-      
+
       // Копируем текущую строку
       for (size_t i = 0; i < size_; ++i) {
         new_ptr[i] = ptr_[i];
       }
-      
+
       // Добавляем другую строку
       for (size_t i = 0; i < other.size_; ++i) {
         new_ptr[size_ + i] = other.ptr_[i];
       }
-      
+
       new_ptr[new_size] = T(0);
       delete[] ptr_;
       ptr_ = new_ptr;
@@ -171,12 +172,12 @@ for (size_t i = 0; symbol != 0; ++i) {
   basic_string& operator+=(T symbol) {
     if (symbol != T(0)) {
       T* new_ptr = new T[size_ + 2];
-      
+
       // Копируем текущую строку
       for (size_t i = 0; i < size_; ++i) {
         new_ptr[i] = ptr_[i];
       }
-      
+
       new_ptr[size_] = symbol;
       new_ptr[size_ + 1] = T(0);
       delete[] ptr_;
@@ -186,13 +187,9 @@ for (size_t i = 0; symbol != 0; ++i) {
     return *this;
   }
 
-  T& operator[](size_t index) noexcept {
-    return ptr_[index];
-  }
+  T& operator[](size_t index) noexcept { return ptr_[index]; }
 
-  const T& operator[](size_t index) const noexcept {
-    return ptr_[index];
-  }
+  const T& operator[](size_t index) const noexcept { return ptr_[index]; }
 
   T& at(size_t index) {
     if (index >= size_) {
@@ -209,8 +206,10 @@ private:
   }
 
   static size_t strlen_(const T* str) {
-    if (!str) return 0;
-    
+    if (!str) {
+      return 0;
+    }
+
     size_t len = 0;
     while (str[len] != T(0)) {
       ++len;
@@ -221,5 +220,4 @@ private:
   T* ptr_;
   size_t size_;
 };
-}
-// namespace bmstu
+}  // namespace bmstu
